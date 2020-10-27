cbf816d1d89d6695e0f4c961202ef16ea121b473
keertirajmalik <keertirajmalik@gmail.com>
2020-10-27 20:20:23 +0530

    inwork

---
+++ README.md
@@ -0,0 +1,3 @@
+# discord--bot
+Just trying to learn how to create the discord bot using discord.js library
+
---
+++ src/Message.js
@@ -0,0 +1,6 @@
+export default class Message{
+    constructor(chatRoom, content){
+        this.chatRoom = chatRoom;
+        this.content = content;
+      }
+}
